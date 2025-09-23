6e30a6c585883bd5d20973598604108d6402172f
Nndamulele Casper Tshipapa <nndamulelechip18@gmail.com>
2025-09-23 22:44:40 +0200

    globe removed for speed

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -54,7 +54,7 @@ const About = () => {
                 <div className='col-span-1 xl:row-span-4'>
                     <div className='grid-container'>
                         <div className='rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center'>
-                            <Suspense fallback={<LoadingGlobe />}>
+                            {/* <Suspense fallback={<LoadingGlobe />}>
                                 <Globe
                                     height={326}
                                     width={326}
@@ -65,7 +65,7 @@ const About = () => {
                                     globeImageUrl='//unpkg.com/three-globe/example/img/earth-night.jpg'
                                     bumpImageUrl='//unpkg.com/three-globe/example/img/earth-topology.png'
                                 />
-                            </Suspense>
+                            </Suspense> */}
                         </div>
                         <div>
                             <p className='grid-headtext'>Location</p>
